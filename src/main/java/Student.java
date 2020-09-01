87360b56ff059e76aa332c8c0ba91d89fd6760ee
Amaro Terrazas <42896111+AmaroT@users.noreply.github.com>
2020-09-01 13:59:14 -0500

    push1

---
+++ src/main/java/Student.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+
+public class Student {
+    public static long id;
+    public static String name;
+    public static ArrayList<Integer> grades;
+    //returns the student's id
+
+    //returns the student's name
+
+    //adds the given grade to the grades list
+
+    //returns the list of grades
+
+    //returns the average of the students grades
+
+    public static long getId() {
+        return id;
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    public static ArrayList<Integer> getGrades() {
+        return grades;
+    }
+
+    public static void setId(long id) {
+        Student.id = id;
+    }
+
+    public static void setName(String name) {
+        Student.name = name;
+    }
+
+    public static void setGrades(ArrayList<Integer> grades) {
+        Student.grades = grades;
+    }
+
+
+
+}
